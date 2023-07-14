73d6ca32a809193e124e3273faf7d64b8c285466
Puza24 <okmanovic.ognjen@gmail.comgit config --global user.email dokmanovic.ognjen@gmail.com>
2023-07-14 17:11:25 +0200

    feature-2: uncomment code

--- cypress/tests/specs/smoke_test.js
+++ cypress/tests/specs/smoke_test.js
@@ -5,14 +5,14 @@ describe("Smoke test Bloxico Website", () => {
     beforeEach(() => {
         cy.visit("https://bloxico.com/")
     })
-    // it("Check if you are at Bloxico Website", () => {
-    //     cy.get(".elementor-heading-title:first-of-type:nth-of-type(1)").should("be.visible")
-    //     cy.contains("Second to none").should('be.visible') 
-    // })
-    // it("Click on Let's get started button", () => {
-    //     cy.contains("LET'S GET STARTED").click()
-    //     cy.contains('Home • Services').should('be.visible')
-    // })
+    it("Check if you are at Bloxico Website", () => {
+        cy.get(".elementor-heading-title:first-of-type:nth-of-type(1)").should("be.visible")
+        cy.contains("Second to none").should('be.visible') 
+    })
+    it("Click on Let's get started button", () => {
+        cy.contains("LET'S GET STARTED").click()
+        cy.contains('Home • Services').should('be.visible')
+    })
     it("Check Services dropdown", () => {
         cy.contains("LET'S GET STARTED").click()
         cy.get('.elementor-menu-toggle__icon--open').click()
