6e2c8dac958433e7160b7468f37c576678e66bf8
dunjabb <dunja.boskovic@bloxico.com>
2023-07-27 17:28:03 +0200

    feature-3: WIP

--- cypress/tests/specs/checkFooterSection.js
+++ cypress/tests/specs/checkFooterSection.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+
 describe("Check Footer Section", () => {
 
     beforeEach(() => {
@@ -7,9 +9,13 @@ describe("Check Footer Section", () => {
         cy.get('a[href="https://bloxico.com/"]:visible').eq(1).click()
         cy.url().should('include', 'bloxico')
     })
-    // it("Check Let's get in touch section", () => {
-    //     cy.contains("LET'S GET IN TOUCH!").should('be.visible')
-    // })
+    it("Check Let's get in touch section", () => {
+        cy.contains("LET’S GET IN TOUCH!").should('be.visible')
+        cy.contains('.elementor-element-7b85885', 'Veljka Dugoševića').should('be.visible')
+    })
+    it("Check contact email address", () => {
+        cy.get('a[href="mailto:contact@bloxico.com"]').eq(0).should('not.be.disabled')
+    })
     it("Check Contact Us button", () => {
         cy.contains('.elementor-button-content-wrapper', 'CONTACT US').find('.elementor-button-text').click()
         cy.url().should('include', 'contact')
@@ -22,4 +28,22 @@ describe("Check Footer Section", () => {
         cy.get('a[href="https://bloxico.com/privacy-policy/"]:visible').eq(0).click()
         cy.url().should('include', 'privacy-policy')
     })
+    // it.only("Check Servies menu hyperlink redirections", () => {
+    //     cy.get('a[style="color: #fff;"]').each(($link, index) => {
+    //         if (index < 4) {
+    //             const href = $link.attr('href')
+    //             cy.wrap($link).click()
+    //             cy.url().should('include', href)
+    //         }
+    //     })
+    // })
+    // it("Check Projects menu hyperlink redirections", () => {
+    //     cy.get('a[style="color: #fff;"]').each(($link, index) => {
+    //         if (index > 4 || index < 11) {
+    //             const href = $link.attr('href')
+    //             cy.wrap($link).click()
+    //             cy.url().should('include', href)
+    //         }
+    //     })
+    // })
 })
