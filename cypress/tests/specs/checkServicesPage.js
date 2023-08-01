062fc0c221c919e1a67c93c3cbca312cbf129249
dunjabb <dunja.boskovic@bloxico.com>
2023-08-02 00:14:37 +0200

    feature-4: Check Services page Test

--- cypress/tests/specs/checkServicesPage.js
+++ cypress/tests/specs/checkServicesPage.js
@@ -15,11 +15,11 @@ describe("Check Services page", () => {
         cy.get('@HeaderText').eq(2).contains('SERVICE BREAKDOWN')
         for (let index = 4; index <= 6; index++) {
             cy.get('@HeaderText').eq(index).should(($element) => {
-                const expectedTexts = ['Software Development', 'IT Outsourcing', 'Blockchain consulting'];
+                const expectedTexts = ['Software Development', 'IT Outsourcing', 'Blockchain consulting']
                 expect($element.text()).to.equal(expectedTexts[index - 4])
             })
         }
-        // read more/read less button and text assertion
+        cy.get('.elementor-toggle-title').should('not.be.disabled').click()
     })
     it("Check How we do it Services section", () => {
         cy.get('h2[class="elementor-heading-title elementor-size-default"]').as('HeaderText')
@@ -27,6 +27,7 @@ describe("Check Services page", () => {
         cy.get('a[class="elementor-button elementor-button-link elementor-size-lg"]').as('sectionButton')
         // cy.get('@sectionButton').eq(0).invoke('removeAttr', 'target').click()
         // cy.url().should('include', 'Bloxico-RSSP-Procedure')
+        // cy.go('back')
         cy.get('@sectionButton').eq(1).click()
         cy.url().should('include', 'contact')
         cy.go('back')
